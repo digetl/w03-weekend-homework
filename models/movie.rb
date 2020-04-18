157915ea54db8a7237bd73190e3ab38eac9486a1
Dave Campbell <dave@digetl.co.uk>
2020-04-18 11:25:05 +0100

    Added init and save to Movie class

--- models/movie.rb
+++ models/movie.rb
@@ -2,4 +2,32 @@ require_relative("../db/sql_runner")
 
 class Movie
 
+    attr_reader :id
+    attr_accessor :title, :price
+
+    def initialize(options)
+        @id = options['id'].to_i if options['id']
+        @title = options['title']
+        @price = options['price']
+    end
+
+    def save()
+        sql = "INSERT INTO
+        moives
+        (
+            title,
+            price
+            )
+            VALUES
+            (
+                $1, $2
+            )
+            RETURNING id"
+            values = [@title, @price]
+            movie = SqlRunner.run(sql, values).first
+            @id = movie['id'].to_i
+    end
+    
+
+
 end
